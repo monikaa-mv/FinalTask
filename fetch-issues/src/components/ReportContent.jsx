9de38c3675ea274eca20ea058ff38a9fbdce49bd
monikaa-mv <114807476+monikaa-mv@users.noreply.github.com>
2023-05-31 15:42:40 +0530

    Add files via upload

---
+++ fetch-issues/src/components/ReportContent.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReportRow from "./ReportRow";
+
+const ReportContent = ({ issues, issueslink, StoreArray }) => {
+  return (
+    <tbody>
+      {issues.map((issueData) => {
+        return <ReportRow issueData={issueData}
+        StoreArray={StoreArray} />;
+      })}
+    </tbody>
+  );
+};
+
+export default ReportContent;
